01c325102401265d5625ec6fc61f0356a20f622a
200466543 <200466543@student.georgianc.on.ca>
2021-01-23 11:30:29 -0500

    Did challenge 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner Scan = new Scanner(System.in);
+        /*Scanner Scan = new Scanner(System.in);
         System.out.println("Type Something");
 
         String input = Scan.nextLine();
         System.out.println("You typed: " + input);
 
-        System.out.println("Added a line of text");
+       System.out.println("Added a line of text");*/
+
+        /*for (int i = 100; i >= 0; i= i-5) {
+            System.out.println(i);
+        }*/
+
+        for (int i = 0; i <= 100; i= i+10) {
+            System.out.print(i);
+            if(i/20 >=1){
+                System.out.println(" Java");
+            }
+            System.out.println("");
+
+        }
+
+
 
     }
 }
