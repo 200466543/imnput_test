534c047cb11897d446d11a93f5f530d47ed52615
200466543 <200466543@student.georgianc.on.ca>
2021-01-23 10:03:24 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Type Something");
+
+        String input = Scan.nextLine();
+        System.out.println("You typed: " + input);
+
+    }
+}
